a2d59e7375a31e940f42b3eccbf72474c8eadff4
mazliahdan-ctrl <mazliahdan@gmail.com>
2026-01-09 17:22:07 +0200

    Update image processing with edge detection and scoring
    Refactor image processing to include edge detection and score calculation.

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
-
 from skimage.filters import median
 from skimage.morphology import ball
-
 from image_utils import load_image, edge_detection
+import numpy as np
 
 
-
-image_path = "/content/my_cat.jpeg"
-image = load_image(image_path)
+image = load_image('.tests/lena.jpg')
 
 
-clean_image = median(image, ball(3))
+image = median(image, ball(3))
 
 
-edgeMAG = edge_detection(clean_image)
+edge = edge_detection(image)
 
 
-plt.figure()
-plt.hist(edgeMAG.ravel(), bins=256)
-plt.title("Edge magnitude histogram")
-plt.show()
+edge_binary = edge > 50
 
-threshold = 50
-edge_binary = edgeMAG > threshold   # boolean array
 
-plt.figure()
-plt.imshow(edge_binary, cmap="gray")
-plt.axis("off")
-plt.show()
+true = load_image('.tests/lena_edges.png')
+if true.ndim == 3:
+    true = np.mean(true, axis=2)  
 
-edge_binary_uint8 = (edge_binary.astype(np.uint8)) * 255
-edge_image = Image.fromarray(edge_binary_uint8)
-edge_image.save("my_edges.png")
+
+area = true.shape[0] * true.shape[1]
+score = np.sum(true == edge_binary) / area
+assert score > 0.9
