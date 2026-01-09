b084cdd35d33bcb6ddafca176eb54121c74bd7a6
mazliahdan-ctrl <mazliahdan@gmail.com>
2026-01-09 17:07:55 +0200

    Refactor image loading and edge detection kernels
    Updated load_image function to accept a file path and modified edge detection to use new kernel variables.

--- image_utils.py
+++ image_utils.py
@@ -3,53 +3,35 @@ import numpy as np
 from scipy.ndimage import convolve
 
 
-
 def load_image(file_path):
-    my_cat = "my_cat.jpeg"
-    cat = Image.open(my_cat).convert("RGB")
-    image_array = np.array(cat)
+   
+    image = Image.open(file_path).convert("RGB")
+    image_array = np.array(image)
     return image_array
-
-image_path = "/content/my_cat.jpeg"
-img_array = load_image(image_path)
-
-print(type(img_array))
-print(img_array.shape)
-print(img_array.dtype)
-
 
 
 def edge_detection(image_array):
     
-    grayscale_image = np.mean(image_array, axis=2)
 
-    
-    kY = np.array([
-        [1, 2, 1],
-        [0, 0, 0],
+   
+    kernelY = np.array([
+        [1,  2,  1],
+        [0,  0,  0],
         [-1, -2, -1]
     ])
 
-    
-    kX = np.array([
+    kernelX = np.array([
         [-1, 0, 1],
         [-2, 0, 2],
         [-1, 0, 1]
     ])
 
     
-    edgeY = convolve(grayscale_image, kY, mode='constant', cval=0)
-    edgeX = convolve(grayscale_image, kX, mode='constant', cval=0)
+    edgeY = convolve(grayscale_image, kernelY, mode="constant", cval=0.0)
+    edgeX = convolve(grayscale_image, kernelX, mode="constant", cval=0.0)
 
    
     edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
 
     return edgeMAG
 
-
-edge_magnitudes = edge_detection(img_array)
-
-print(f"Shape of edgeMAG: {edge_magnitudes.shape}")
-print(f"Data type of edgeMAG: {edge_magnitudes.dtype}")
-print(f"Min value of edgeMAG: {np.min(edge_magnitudes)}")
-print(f"Max value of edgeMAG: {np.max(edge_magnitudes)}")
