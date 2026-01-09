17bbb1f4cb5fadf10a127c0e13745727509ec984
mazliahdan-ctrl <mazliahdan@gmail.com>
2026-01-09 17:21:11 +0200

    Refactor image loading and edge detection functions

--- image_utils.py
+++ image_utils.py
@@ -4,36 +4,28 @@ from scipy.ndimage import convolve
 
 
 def load_image(file_path):
-   
-    image = Image.open(file_path).convert("RGB")
-    image_array = np.array(image)
-    return image_array
+    
+    img = Image.open(file_path).convert("RGB")
+    return np.array(img)
 
 
 def edge_detection(image_array):
-   
-
     
     grayscale_image = np.mean(image_array, axis=2)
 
- 
-    kernelY = np.array([
-        [1,  2,  1],
-        [0,  0,  0],
-        [-1, -2, -1]
-    ])
+    
+    kernelY = np.array([[1,2,1],[0,0,0],[-1,-2,-1]])
+    kernelX = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
 
-    kernelX = np.array([
-        [-1, 0, 1],
-        [-2, 0, 2],
-        [-1, 0, 1]
-    ])
+    
+    edgeY = convolve(grayscale_image, kernelY, mode='constant', cval=0.0)
+    edgeX = convolve(grayscale_image, kernelX, mode='constant', cval=0.0)
 
-   
-    edgeY = convolve(grayscale_image, kernelY, mode="constant", cval=0.0)
-    edgeX = convolve(grayscale_image, kernelX, mode="constant", cval=0.0)
-
-   
+    
     edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
 
     return edgeMAG
+
+
+   
+  
