8a61372410c96d843bf87c74bafa04bb98ece2c9
mazliahdan-ctrl <mazliahdan@gmail.com>
2026-01-09 15:04:14 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,9 +1,55 @@
 from PIL import Image
 import numpy as np
-from scipy.signal import convolve2d
+from scipy.ndimage import convolve
 
-def load_image(path):
-    pass # Replace the `pass` with your code
 
-def edge_detection(image):
-    pass # Replace the `pass` with your code
+
+def load_image(file_path):
+    my_cat = "my_cat.jpeg"
+    cat = Image.open(my_cat).convert("RGB")
+    image_array = np.array(cat)
+    return image_array
+
+image_path = "file_path.jpg"
+img_array = load_image(image_path)
+
+print(type(img_array))
+print(img_array.shape)
+print(img_array.dtype)
+
+
+
+def edge_detection(image_array):
+    
+    grayscale_image = np.mean(image_array, axis=2)
+
+    
+    kY = np.array([
+        [1, 2, 1],
+        [0, 0, 0],
+        [-1, -2, -1]
+    ])
+
+    
+    kX = np.array([
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1]
+    ])
+
+    
+    edgeY = convolve(grayscale_image, kY, mode='constant', cval=0)
+    edgeX = convolve(grayscale_image, kX, mode='constant', cval=0)
+
+   
+    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+
+    return edgeMAG
+
+
+edge_magnitudes = edge_detection(img_array)
+
+print(f"Shape of edgeMAG: {edge_magnitudes.shape}")
+print(f"Data type of edgeMAG: {edge_magnitudes.dtype}")
+print(f"Min value of edgeMAG: {np.min(edge_magnitudes)}")
+print(f"Max value of edgeMAG: {np.max(edge_magnitudes)}")
