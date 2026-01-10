fed86230d2315230c5e4c7f2ce5e3f2e01fef562
mazliahdan-ctrl <mazliahdan@gmail.com>
2026-01-10 17:48:22 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,30 +1,43 @@
 from PIL import Image
 import numpy as np
-from scipy.ndimage import convolve
+
+
+
+
+from scipy.signal import convolve2d
 
 
 def load_image(file_path):
-    
-    img = Image.open(file_path).convert("RGB")
-    return np.array(img)
+   
+    image = Image.open(file_path)
+    image_array = np.array(image)
+    return image_array
 
 
 def edge_detection(image_array):
+   
+
     
     grayscale_image = np.mean(image_array, axis=2)
 
-    
-    kernelY = np.array([[1,2,1],[0,0,0],[-1,-2,-1]])
-    kernelX = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+ 
+    kernelY = np.array([[1,  2,  1],
+                        [0,  0,  0],
+                        [-1, -2, -1]])
 
-    
-    edgeY = convolve(grayscale_image, kernelY, mode='constant', cval=0.0)
-    edgeX = convolve(grayscale_image, kernelX, mode='constant', cval=0.0)
+    kernelX = np.array([[-1, 0, 1],
+                        [-2, 0, 2],
+                        [-1, 0, 1]])
 
-    
-    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+   
+    edgeY = convolve2d(grayscale_image, kernelY, mode="same" )
+    edgeX = convolve2d(grayscale_image, kernelX, mode="same")
+
+   
+    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)**0.5
 
     return edgeMAG
+
 
 
    
