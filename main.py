f4a1086adb1473316391dda8d38d9ff6c62ba7d3
mazliahdan-ctrl <mazliahdan@gmail.com>
2026-01-10 17:49:35 +0200

    Refactor image processing with new image and edge detection
    Updated image loading and processing logic to use 'my_cat.jpeg'. Added edge detection on the cleaned image and displayed the binary edge image.

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 from skimage.filters import median
-from skimage.morphology import ball
+from skimage.morphology import ball 
+
 from image_utils import load_image, edge_detection
-import numpy as np
 
 
-image = load_image('.tests/lena.jpg')
 
 
-image = median(image, ball(3))
+image = load_image("my_cat.jpeg")
 
 
-edge = edge_detection(image)
+
+clean_image = median(image, ball(3))
+edge_img = edge_detection(clean_image)
 
 
-edge_binary = edge > 50
 
+edge_binary = edge_img > 50
 
-true = load_image('.tests/lena_edges.png')
-if true.ndim == 3:
-    true = np.mean(true, axis=2)  
-
-
-area = true.shape[0] * true.shape[1]
-score = np.sum(true == edge_binary) / area
-assert score > 0.9
+plt.imshow(edge_binary, cmap="gray")
